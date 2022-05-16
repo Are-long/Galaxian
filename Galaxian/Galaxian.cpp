12075e41504643bf1e10e0f27f81da3fbd7d6c43
zhenggitlong <486479574@qq.com>
2022-05-16 10:06:40 +0800

    添加项目文件。

---
+++ Galaxian/Galaxian.cpp
@@ -0,0 +1,207 @@
+﻿// Galaxian.cpp : 定义应用程序的入口点。
+//
+#define  _CRT_SECURE_NO_WARNINGS
+#include "framework.h"
+#include "Galaxian.h"
+//#include"object.h"
+#include<time.h>
+#include<windows.h>
+#include<stdlib.h>
+#include<background.h>
+#pragma comment(lib,"Winmm.lib")
+#define MAX_LOADSTRING 100
+
+// 全局变量:
+//HWND		hwnd_main;
+HINSTANCE hInst;                                // 当前实例
+WCHAR szTitle[MAX_LOADSTRING];                  // 标题栏文本
+WCHAR szWindowClass[MAX_LOADSTRING];            // 主窗口类名
+HBITMAP		hbmp_cover, hbmp_object;
+BITMAP		bmp_cover, bmp_object;
+HDC			hdc_cover, hdc_object;
+SIZE		sz_scr = { GetSystemMetrics(SM_CXSCREEN),GetSystemMetrics(SM_CYSCREEN) };//获取屏幕大小
+int         game_level = 0;
+RECT        rectDlg;
+// 此代码模块中包含的函数的前向声明:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: 在此处放置代码。
+
+    // 初始化全局字符串
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_GALAXIAN, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // 执行应用程序初始化:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_GALAXIAN));
+
+    MSG msg;
+
+    // 主消息循环:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  函数: MyRegisterClass()
+//
+//  目标: 注册窗口类。
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_GALAXIAN));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_GALAXIAN);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   函数: InitInstance(HINSTANCE, int)
+//
+//   目标: 保存实例句柄并创建主窗口
+//
+//   注释:
+//
+//        在此函数中，我们在全局变量中保存实例句柄并
+//        创建和显示主程序窗口。
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // 将实例句柄存储在全局变量中
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW| WS_THICKFRAME,
+      CW_USEDEFAULT, 0, 775, 723, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+   hbmp_cover = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_COVER));
+   hbmp_object = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_OBJECT));
+   GetObject(hbmp_cover, sizeof(bmp_cover), &bmp_cover);
+   GetObject(hbmp_object, sizeof(bmp_object), &bmp_object);
+   HDC dc = GetDC(hWnd);
+   hdc_cover = CreateCompatibleDC(dc);
+   hdc_object = CreateCompatibleDC(dc);
+   SelectObject(hdc_cover, hbmp_cover);
+   SelectObject(hdc_object, hbmp_object);
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  函数: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  目标: 处理主窗口的消息。
+//
+//  WM_COMMAND  - 处理应用程序菜单
+//  WM_PAINT    - 绘制主窗口
+//  WM_DESTROY  - 发送退出消息并返回
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // 分析菜单选择:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            GetWindowRect(hWnd, &rectDlg);//获取目标窗口大小
+
+            //StretchBlt(hdc, 0, 0, sz_scr.cx-(rectDlg.right- rectDlg.left), sz_scr.cy - (rectDlg.top - rectDlg.bottom),
+            //hdc_cover, 0, 0, sz_scr.cx - (rectDlg.right - rectDlg.left), sz_scr.cy - (rectDlg.top - rectDlg.bottom), SRCCOPY);
+             StretchBlt(hdc, 0, 0, 16, 16,hdc_object, 0, 0, 16, 16, SRCCOPY);
+            // TODO: 在此处添加使用 hdc 的任何绘图代码...
+           
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// “关于”框的消息处理程序。
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {   
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
+
+
---
+++ Galaxian/object.h
@@ -0,0 +1,24 @@
+#define MAX_MAP_X 100
+#define MAX_MAP_Y 100
+
+int map[MAX_MAP_Y][MAX_MAP_X];
+
+SIZE sz_block, sz_map, sz_bullet;
+RECT loc_map;
+POINT pt_play1, pt_play2;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
